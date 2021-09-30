f032cf5e951fcd78be32c85cfc93989e750214ac
kghugo <hugosum.dev@protonmail.com>
2021-09-30 19:16:43 +0100

    Complete task 1 and 2

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "vim training demo";
+
+  outputs = { self, nixpkgs }:
+    let system = "x86_64-linux";
+    in {
+      devShell.${system} = (({ pkgs, ... }:
+        pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodePackages.node2nix
+            nodejs
+          ];
+
+          shellHook = ''
+          '';
+        }) { pkgs = nixpkgs.legacyPackages.${system}; });
+    };
+}
