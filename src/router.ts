d644b37f725dbeb492385ea21e5ea88b390e6f61
kghugo <hugosum.dev@protonmail.com>
2021-10-02 09:07:50 +0100

    Protect search route

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import LoginForm from "./LoginForm.vue";
 import SearchFlim from "./SearchFilm.vue";
+import store from './store'
 
 export default createRouter({
   history: createWebHistory(),
@@ -15,6 +16,12 @@ export default createRouter({
       path: "/search",
       name: "search-film",
       component: SearchFlim,
+      beforeEnter(to, from, next){
+          if (store.state.loggedIn) {
+            return next()
+          }
+          next({ path: "/" })
+      }
     },
   ],
 });
